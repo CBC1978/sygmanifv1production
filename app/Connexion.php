8e8a77d1fc37ef0f427e14fa8746e0fba87c16a9
dolidev2 <dolidev2.0@gmail.com>
2025-04-16 10:40:40 +0000

    fix error to user in connection file

--- app/Connexion.php
+++ app/Connexion.php
@@ -1,7 +1,7 @@
 <?php
 // Connexion à la base de données
 function bdd() {
-$mysqli = new mysqli("mysql_db", "cbc", 'Cbcfaso1978', "sygmanifv2prod");
+$mysqli = new mysqli("mysql_db", "cbc", 'Cbcfaso@1978', "sygmanifv2prod");
 	if ($mysqli->connect_errno) {
     echo "Echec lors de la connexion à MySQL : (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 	}
